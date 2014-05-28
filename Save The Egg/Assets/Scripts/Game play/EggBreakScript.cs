58801979442118dc3f91344db72d4c26a927502f
Levi <tribune1216@gmail.com>
2014-05-28 11:21:39 +0800

    levi commit 52814 11:21AM

--- Save The Egg/Assets/Scripts/Game play/EggBreakScript.cs
+++ Save The Egg/Assets/Scripts/Game play/EggBreakScript.cs
@@ -3,16 +3,21 @@ using System.Collections;
 
 public class EggBreakScript : MonoBehaviour {
 	
-	GameObject spawnBrokenEgg;
-	ObjectPooler brokenEgg;
+	GameObject spawnWhiteBrokenEgg, spawnGoldBrokenEgg;
+	ObjectPooler whitebrokenEgg, goldbrokenEgg, redbrokenEgg, bluebrokenEgg;
 	public GameObject BreakEgg;
 
 	void OnCollisionEnter(Collision other) {
 		if(other.gameObject.name == "Floor" || other.gameObject.name == "Cart" || other.gameObject.name == "basketwall"){
-			brokenEgg =  GameObject.Find("brokenEggPooler").GetComponent<ObjectPooler>();
-			spawnBrokenEgg = brokenEgg.GetPooledObject();
-			spawnBrokenEgg.transform.position = new Vector3(gameObject.transform.position.x - 1, gameObject.transform.position.y, gameObject.transform.position.z);
-			spawnBrokenEgg.SetActive(true);
+			whitebrokenEgg =  GameObject.Find("brokenEggPooler").GetComponent<ObjectPooler>();
+			spawnWhiteBrokenEgg = whitebrokenEgg.GetPooledObject();
+			spawnWhiteBrokenEgg.transform.position = new Vector3(BreakEgg.transform.position.x - 1, BreakEgg.transform.position.y, BreakEgg.transform.position.z);
+			spawnWhiteBrokenEgg.SetActive(true);
+			Invoke("RemoveBrokenEgg",5f);
 		}
+	}
+
+	void RemoveBrokenEgg(){
+		spawnWhiteBrokenEgg.SetActive(false);
 	}
 }
