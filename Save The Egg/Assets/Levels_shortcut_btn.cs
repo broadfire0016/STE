f76b2bbf75ba48ee5901a2720a19c665ff1a9eb9
broadfire0016 <tribune1216@gmail.com>
2014-05-29 03:40:30 +0800

    levi 52914 3:39 AM ->Iphone 5 Res,Different Broken Eggs, Level Shortcuts

--- Save The Egg/Assets/Levels_shortcut_btn.cs
+++ Save The Egg/Assets/Levels_shortcut_btn.cs
@@ -164,14 +164,71 @@ public class Levels_shortcut_btn : MonoBehaviour {
 
 #if UNITY_IOS
 		if (Screen.width == 640 && Screen.height == 960) {
-			
+			L1.positionFromTopLeft (0.10f, 0.30f);
+			L1.setSize(L1.width/ scaleFactor, L1.height / scaleFactor * 0.7f);
+			L2.positionFromTopLeft (0.17f, 0.30f);
+			L2.setSize(L2.width/ scaleFactor, L2.height / scaleFactor * 0.7f);
+			L3.positionFromTopLeft (0.24f, 0.30f);
+			L3.setSize(L3.width/ scaleFactor, L3.height / scaleFactor * 0.7f);
+			L4.positionFromTopLeft (0.31f, 0.30f);
+			L4.setSize(L4.width/ scaleFactor, L4.height / scaleFactor * 0.7f);
+			L5.positionFromTopLeft (0.38f, 0.30f);
+			L5.setSize(L5.width/ scaleFactor, L5.height / scaleFactor * 0.7f);
+			L6.positionFromTopLeft (0.45f, 0.30f);
+			L6.setSize(L6.width/ scaleFactor, L6.height / scaleFactor * 0.7f);
+			L7.positionFromTopLeft (0.52f, 0.30f);
+			L7.setSize(L7.width/ scaleFactor, L7.height / scaleFactor * 0.7f);
+			L8.positionFromTopLeft (0.59f, 0.30f);
+			L8.setSize(L8.width/ scaleFactor, L8.height / scaleFactor * 0.7f);
+			L9.positionFromTopLeft (0.66f, 0.30f);
+			L9.setSize(L9.width/ scaleFactor, L9.height / scaleFactor * 0.7f);
+			L10.positionFromTopLeft (0.73f, 0.30f);
+			L10.setSize(L10.width/ scaleFactor, L10.height / scaleFactor * 0.7f);
 		}
 		if (Screen.width == 768 && Screen.height == 1024 || Screen.width == 1536 && Screen.height == 2048) {
-			
+			L1.positionFromTopLeft (0.10f, 0.30f);
+			L1.setSize(L1.width/ scaleFactor, L1.height / scaleFactor * 0.7f);
+			L2.positionFromTopLeft (0.17f, 0.30f);
+			L2.setSize(L2.width/ scaleFactor, L2.height / scaleFactor * 0.7f);
+			L3.positionFromTopLeft (0.24f, 0.30f);
+			L3.setSize(L3.width/ scaleFactor, L3.height / scaleFactor * 0.7f);
+			L4.positionFromTopLeft (0.31f, 0.30f);
+			L4.setSize(L4.width/ scaleFactor, L4.height / scaleFactor * 0.7f);
+			L5.positionFromTopLeft (0.38f, 0.30f);
+			L5.setSize(L5.width/ scaleFactor, L5.height / scaleFactor * 0.7f);
+			L6.positionFromTopLeft (0.45f, 0.30f);
+			L6.setSize(L6.width/ scaleFactor, L6.height / scaleFactor * 0.7f);
+			L7.positionFromTopLeft (0.52f, 0.30f);
+			L7.setSize(L7.width/ scaleFactor, L7.height / scaleFactor * 0.7f);
+			L8.positionFromTopLeft (0.59f, 0.30f);
+			L8.setSize(L8.width/ scaleFactor, L8.height / scaleFactor * 0.7f);
+			L9.positionFromTopLeft (0.66f, 0.30f);
+			L9.setSize(L9.width/ scaleFactor, L9.height / scaleFactor * 0.7f);
+			L10.positionFromTopLeft (0.73f, 0.30f);
+			L10.setSize(L10.width/ scaleFactor, L10.height / scaleFactor * 0.7f);
 			
 		}
 		if (Screen.width == 640 && Screen.height == 1136){
-			
+			L1.positionFromTopLeft (0.10f, 0.30f);
+			L1.setSize(L1.width/ scaleFactor, L1.height / scaleFactor * 0.7f);
+			L2.positionFromTopLeft (0.17f, 0.30f);
+			L2.setSize(L2.width/ scaleFactor, L2.height / scaleFactor * 0.7f);
+			L3.positionFromTopLeft (0.24f, 0.30f);
+			L3.setSize(L3.width/ scaleFactor, L3.height / scaleFactor * 0.7f);
+			L4.positionFromTopLeft (0.31f, 0.30f);
+			L4.setSize(L4.width/ scaleFactor, L4.height / scaleFactor * 0.7f);
+			L5.positionFromTopLeft (0.38f, 0.30f);
+			L5.setSize(L5.width/ scaleFactor, L5.height / scaleFactor * 0.7f);
+			L6.positionFromTopLeft (0.45f, 0.30f);
+			L6.setSize(L6.width/ scaleFactor, L6.height / scaleFactor * 0.7f);
+			L7.positionFromTopLeft (0.52f, 0.30f);
+			L7.setSize(L7.width/ scaleFactor, L7.height / scaleFactor * 0.7f);
+			L8.positionFromTopLeft (0.59f, 0.30f);
+			L8.setSize(L8.width/ scaleFactor, L8.height / scaleFactor * 0.7f);
+			L9.positionFromTopLeft (0.66f, 0.30f);
+			L9.setSize(L9.width/ scaleFactor, L9.height / scaleFactor * 0.7f);
+			L10.positionFromTopLeft (0.73f, 0.30f);
+			L10.setSize(L10.width/ scaleFactor, L10.height / scaleFactor * 0.7f);
 		}
 
 #endif	
---
+++ Save The Egg/Assets/test1.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("Width " + Screen.width + " Height " + Screen.height);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
