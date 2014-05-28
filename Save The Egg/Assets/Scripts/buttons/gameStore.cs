8e599c7758ee3d27d5334b6eeaa0ca4cb3510840
dalawatatlo <aiza.a@poddcorp.com>
2014-05-28 11:51:20 +0800

    aiza 52814 11:51am

--- Save The Egg/Assets/Scripts/buttons/gameStore.cs
+++ Save The Egg/Assets/Scripts/buttons/gameStore.cs
@@ -9,35 +9,39 @@ public class gameStore : MonoBehaviour {
 	void Start () {
 		var scaleFactor = ScaleFactor.GetScaleFactor ();
 		var backButton = UIButton.create(backManager, "back_normal2.png","back_active2.png",0,0);
-		backButton.positionFromBottomLeft( 0.08f, 0.42f );
 		backButton.highlightedTouchOffsets = new UIEdgeOffsets(30);
 		backButton.touchDownSound = Click;
-		backButton.setSize(backButton.width / scaleFactor, backButton.height / scaleFactor);
 		backButton.onTouchUpInside += sender => Application.LoadLevel("AGAIN");
 
 
 #if UNITY_EDITOR
 		if (Screen.width == 487 && Screen.height == 730) { // iphone 4
-
+			backButton.positionFromBottomLeft( 0.08f, 0.42f );
+			backButton.setSize(backButton.width / scaleFactor, backButton.height / scaleFactor);
 		}
 		if (Screen.width == 548 && Screen.height == 730) { //ipad
-
+			backButton.positionFromBottomLeft( 0.08f, 0.42f );
+			backButton.setSize(backButton.width / scaleFactor, backButton.height / scaleFactor);
 		}
 		if (Screen.width == 411 && Screen.height == 730){ //iphone 5
-
+			backButton.positionFromBottomLeft( 0.08f, 0.42f );
+			backButton.setSize(backButton.width / scaleFactor, backButton.height / scaleFactor);
 		}	
 		#endif
 		
-		#if UNITY_IOS
+#if UNITY_IOS
 		if (Screen.width == 640 && Screen.height == 960) { // iphone4
-
+			backButton.positionFromBottomLeft( 0.08f, 0.42f );
+			backButton.setSize(backButton.width / scaleFactor, backButton.height / scaleFactor);
 		}
 		if (Screen.width == 768 && Screen.height == 1024 || Screen.width == 1536 && Screen.height == 2048) { //ipad
-
+			backButton.positionFromBottomLeft( 0.08f, 0.42f );
+			backButton.setSize(backButton.width / scaleFactor, backButton.height / scaleFactor);
 		}
 		
 		if (Screen.width == 640 && Screen.height == 1136){ //iphone 5
-
+			backButton.positionFromBottomLeft( 0.08f, 0.42f );
+			backButton.setSize(backButton.width / scaleFactor, backButton.height / scaleFactor);
 		}	
 #endif	
 
