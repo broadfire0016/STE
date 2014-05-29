770e2834b2dc01c221530fe854346c3774ce003f
Levi <tribune1216@gmail.com>
2014-05-29 11:47:04 +0800

    levi 52914 11:47AM

--- Save The Egg/Assets/Scripts/IphonePositioning.cs
+++ Save The Egg/Assets/Scripts/IphonePositioning.cs
@@ -19,8 +19,8 @@ public class IphonePositioning : MonoBehaviour {
 		if (Screen.width == 411 && Screen.height == 730 || Screen.width == 360 && Screen.height == 640){
 			background.transform.localScale = new Vector3(0.83f,background.transform.localScale.y,background.transform.localScale.z);
 			background_pause.transform.localScale = new Vector3(0.83f,background.transform.localScale.y,background.transform.localScale.z);
-			leftChicken.transform.Translate(new Vector3(0f,0f, -0.7f));
-			rightChicken.transform.Translate(new Vector3(0f,0f, 0.7f));
+			leftChicken.transform.Translate(new Vector3(0f,0f, -1.8f));
+			rightChicken.transform.Translate(new Vector3(0f,0f, -1.8f));
 		}
 
 #endif
@@ -30,15 +30,15 @@ public class IphonePositioning : MonoBehaviour {
 		if (Screen.width == 640 && Screen.height == 960) {
 			background.transform.localScale = new Vector3(0.95f,background.transform.localScale.y,background.transform.localScale.z);
 			background_pause.transform.localScale = new Vector3(0.95f,background.transform.localScale.y,background.transform.localScale.z);
-			leftChicken.transform.Translate(new Vector3(0f,0f, -1.05f));
-			rightChicken.transform.Translate(new Vector3(0f,0f, 1.05f));
+			leftChicken.transform.Translate(new Vector3(0f,0f, -0.7f));
+			rightChicken.transform.Translate(new Vector3(0f,0f, 0.7f));
 		}
 
 		if (Screen.width == 640 && Screen.height == 1136){
 			background.transform.localScale = new Vector3(0.83f,background.transform.localScale.y,background.transform.localScale.z);
 			background_pause.transform.localScale = new Vector3(0.83f,background.transform.localScale.y,background.transform.localScale.z);
 			leftChicken.transform.Translate(new Vector3(0f,0f, -1.8f));
-			rightChicken.transform.Translate(new Vector3(0f,0f, 1.8f));
+			rightChicken.transform.Translate(new Vector3(0f,0f,-1.8f));
 		}
 #endif
 	}
