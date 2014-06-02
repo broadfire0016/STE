d6966c7b6b17399c526776a908df9b1e7c98a24f
Levi <tribune1216@gmail.com>
2014-06-02 11:03:35 +0800

    levi 6214 1:03AM

--- Save The Egg/Assets/Scripts/IphonePositioning.cs
+++ Save The Egg/Assets/Scripts/IphonePositioning.cs
@@ -8,11 +8,11 @@ public class IphonePositioning : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 	
-		if (Screen.width == 427 && Screen.height == 640 || Screen.width == 640 && Screen.height == 960) {
-			background.transform.localScale = new Vector3(0.95f,background.transform.localScale.y,background.transform.localScale.z);
-			background_pause.transform.localScale = new Vector3(0.95f,background.transform.localScale.y,background.transform.localScale.z);
-			leftChicken.transform.Translate(new Vector3(0f,0f, -0.7f));
-			rightChicken.transform.Translate(new Vector3(0f,0f, 0.7f));
+		if (Screen.width == 487 && Screen.height == 730 || Screen.width == 640 && Screen.height == 960) {
+			background.transform.localScale = new Vector3(14.60647f,21.85281f,background.transform.localScale.z);
+			background_pause.transform.localScale = new Vector3(0.8611252f,1.056228f,background.transform.localScale.z);
+			leftChicken.transform.Translate(new Vector3(0f,0f, -1.06288f));
+			rightChicken.transform.Translate(new Vector3(0f,0f, 0.79238f));
 		}
 
 		if (Screen.width == 411 && Screen.height == 730 || Screen.width == 360 && Screen.height == 640 || Screen.width == 640 && Screen.height == 1136){
